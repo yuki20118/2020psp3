8c2587b492f0315024a3399bb7d51b0799da205f
fujita yuki <bb35319042@ms.nagasaki-u.ac.jp>
2020-11-26 09:31:26 +0900

    1126

--- k03/k03.c
+++ k03/k03.c
@@ -10,13 +10,51 @@ char StrKey[] = "wind";
 char* ForceSearch(char text[], char key[])
 {
     //  ここを実装する
+    int start = 0;
+    int pos = 0;
+    int text_len=0,key_len=0,i;
 
+    for(i=0; text[i]="\0"; i++){
+        text_len++;
+    }
+
+    for(i=0; key[i]="\0"; i++){
+        key_len++;
+    }
+
+    printf("text_len=%d\n", text_len);
+    printf("key_len=%d\n", key_len);
+
+
+    for(start=0; start <= text_len; start++){
+        for(pos=0; pos <= key_len; pos++){
+            if(text[start + pos]==key[pos]){
+                pos++;
+            }
+            else{
+                pos = 0;
+            }    
+        }
+        
+        
+    }
+    
+    return text+start;
 }
 
 char* BMSearch(char text[], char key[])
 {
     //  ここを実装する
+    /*index = index + table[text[index]]; //ずらし量
 
+
+
+    
+    printf("%s\n", text);               //動作確認
+    for(i=0; i < index - key_len + 1;i++){
+        printf(" ");
+    }
+    printf("%s\n",key);*/
 }
 
 int main(void)
